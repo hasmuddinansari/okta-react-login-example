ca0a10fbc8dea73398d79afc2ef1895444cb1062
Hasmuddin Ansari <46985051+hasmuddinansari@users.noreply.github.com>
2023-05-05 03:56:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-import Login from './Login';
-import Private from './Private';
-import { Route } from 'react-router-dom';
-import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
-import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Login from "./Login";
+import Private from "./Private";
+import { Route } from "react-router-dom";
+import { LoginCallback, SecureRoute, Security } from "@okta/okta-react";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { useHistory } from "react-router-dom";
 
 const oktaAuth = new OktaAuth({
-  issuer: 'https://dev-9323263.okta.com/oauth2/default',
-  clientId: '0oafhhdgdhMDvqaQW5d6',
-  redirectUri: window.location.origin + '/callback'
+  issuer: "https://dev-03935124.okta.com/oauth2/default",
+  clientId: "0oa8li5cyowoCjl7g5d7",
+  redirectUri: window.location.origin + "/login/callback",
 });
 
 const App = () => {
@@ -20,27 +20,29 @@ const App = () => {
   const restoreOriginalUri = async (_oktaAuth, originalUri) => {
     history.replace(toRelativeUrl(originalUri, window.location.origin));
   };
-  const onAuthRequired = function() {
-    history.push('/login')
-  }
+  const onAuthRequired = function () {
+    history.push("/login");
+  };
 
   return (
     <div className="App">
       <div className="page">
         <div className="content">
-          <Security oktaAuth={oktaAuth}
-                    restoreOriginalUri={restoreOriginalUri}
-                    onAuthRequired={onAuthRequired}>
-            <Header/>
-            <Route path='/' exact={true} component={Home}/>
-            <Route path='/login' exact={true} component={Login}/>
-            <SecureRoute path='/private' exact={true} component={Private}/>
-            <Route path='/callback' component={LoginCallback}/>
+          <Security
+            oktaAuth={oktaAuth}
+            restoreOriginalUri={restoreOriginalUri}
+            onAuthRequired={onAuthRequired}
+          >
+            <Header />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/login" exact={true} component={Login} />
+            <SecureRoute path="/private" exact={true} component={Private} />
+            <Route path="/callback" component={LoginCallback} />
           </Security>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
